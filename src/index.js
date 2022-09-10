9d14c306cb5152cf8fdc561ac4066514fd972c0b
FirsovDmitriy <dfirsov@nopreset.ru>
2022-09-10 14:54:57 +0400

    changed root id

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App'
 import './styles/index.scss'
 import store from './store/redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.querySelector('#list_page_holder'))
 root.render(
   <React.StrictMode>
     <Provider store={ store }>
